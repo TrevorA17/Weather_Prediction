05974f52cbeb739de28d7ade09f02bb0ff34ec12
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-06-11 15:50:12 +0300

    load dataset for model training

---
+++ TrainingModel.R
@@ -0,0 +1,11 @@
+# Load necessary library
+library(readr)
+
+# Load dataset
+weather_data <- read.csv("data/seattle-weather.csv", colClasses = c(
+  precipitation = "numeric",
+  temp_max = "numeric",
+  temp_min = "numeric",
+  wind = "numeric",
+  weather = "factor"
+))
